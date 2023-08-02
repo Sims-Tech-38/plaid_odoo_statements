840945962396388af69b0d52ce6ba67777191336
Jacob Sims <Jacob.sims@simstech.cloud>
2023-08-02 12:00:38 -0400

    update

--- models/res_users.py
+++ models/res_users.py
@@ -1,9 +1,7 @@
-# res_users_extension.py
-
 from odoo import fields, models
 
 class ResUsers(models.Model):
     _inherit = 'res.users'
 
-    plaid_access_token = fields.Char(string='Plaid Access Token')
+    plaid_access_token = fields.Char(string='Plaid Access Token', password=True, readonly=True)
     plaid_accounts = fields.One2many('st_odoo_statements.plaid_account', 'user_id', string='Plaid Accounts')
